16873b907044726dbc21e95bb7a52ef82efe5f49
Josh Forbes <jforbes@kimmel.com>
2017-07-09 14:31:18 -0400

    Trashing the set_post method, instead fetching the post when it is actually needed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,47 +1,47 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :update, :destroy]
   skip_before_action :authenticate!, only: [:index, :show]
 
   def index
-    @posts = Post.all
+    posts = Post.all
 
-    render json: @posts
+    render json: posts
   end
 
   def show
-    render json: @post
+    post = Post.find(params[:id])
+
+    render json: post
   end
 
   def create
-    @post = Post.new(post_params)
+    post = Post.new(post_params.merge(user_id: current_user.id))
 
-    if @post.save
-      render json: @post, status: :created, location: @post
+    if post.save
+      render json: post, status: :created
     else
-      render json: @post.errors, status: :unprocessable_entity
+      render json: post.errors, status: :unprocessable_entity
     end
   end
 
   def update
-    if @post.update(post_params)
-      render json: @post
+    post = Post.find(params[:id])
+
+    if post.update(post_params)
+      render json: post
     else
-      render json: @post.errors, status: :unprocessable_entity
+      render json: post.errors, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @post.destroy
+    post = Post.find(params[:id])
+    post.destroy
 
     head :no_content
   end
 
   private
-    def set_post
-      @post = Post.find(params[:id])
-    end
-
     def post_params
-      params.require(:post).permit(:user_id, :title, :body)
+      params.require(:post).permit(:title, :body)
     end
 end
