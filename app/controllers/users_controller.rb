4cf0a050e937089c21a522e4c4d15f9594688cbc
Josh Forbes <jforbes@kimmel.com>
2017-07-09 07:38:33 -0400

    Removing the scaffolded methods we wont be using

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,46 +1,27 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
-  skip_before_action :authenticate!, except: [:index]
-
-  def index
-    @users = User.all
-
-    render json: @users, each_serializer: UserSerializer
-  end
-
-  def show
-    render json: @user, serializer: UserSerializer
-  end
+  skip_before_action :authenticate!, only: [:create]
 
   def create
-    @user = User.new(user_params)
+    user = User.new(user_params)
 
-    if @user.save
-      render json: @user, serializer: UserSerializer, status: :created
+    if user.save
+      render json: user, status: :created
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: user.errors, status: :unprocessable_entity
     end
   end
 
   def update
-    if @user.update(user_params)
-      render json: @user, serializer: UserSerializer
+    user = User.find(params[:id])
+
+    if user.update(user_params)
+      render json: user
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: user.errors, status: :unprocessable_entity
     end
-  end
-
-  def destroy
-    @user.destroy
-
-    head :no_content
   end
 
   private
-    def set_user
-      @user = User.find(params[:id])
-    end
-
     def user_params
       params.require(:user).permit(:email, :name, :token, :password)
     end
--- test/acceptance/users_test.rb
+++ test/acceptance/users_test.rb
@@ -1,15 +1,6 @@
 require 'test_helper'
 
 class UsersTest < ActionDispatch::IntegrationTest
-  setup do
-    @user = create(:user)
-  end
-
-  test 'should get index' do
-    get users_url, as: :json, headers: { 'AUTHORIZATION': @user.token }
-    assert_response :success
-  end
-
   test 'should create user' do
     assert_difference('User.count') do
       post users_url, params: {
@@ -24,21 +15,21 @@ class UsersTest < ActionDispatch::IntegrationTest
     assert_response 201
   end
 
-  test 'should show user' do
-    get user_url(@user), as: :json
-    assert_response :success
-  end
-
   test 'should update user' do
-    patch user_url(@user), params: { user: { email: @user.email, password: @user.password, token: @user.token } }, as: :json
+    user = create(:user)
+
+    patch user_url(user), params: {
+        user: {
+            name: 'New name',
+            email: 'new@email.com',
+            password: 'newPassword',
+        }
+    }, as: :json, headers: { 'AUTHORIZATION': user.token }
+
+    user.reload
+
     assert_response 200
-  end
-
-  test 'should destroy user' do
-    assert_difference('User.count', -1) do
-      delete user_url(@user), as: :json
-    end
-
-    assert_response 204
+    assert_equal('New name', user.name)
+    assert_equal('new@email.com', user.email)
   end
 end
