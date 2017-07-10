626de1206d63e86e11b41fb75a12b09e94482df6
Josh Forbes <jforbes@kimmel.com>
2017-07-09 20:20:33 -0400

    Adding unit tests around the password rules on a user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,9 @@ class User < ApplicationRecord
   has_secure_password
   has_secure_token :token
 
-  validates :name, :email, :password, presence: true
+  validates :name, :email, presence: true
   validates :name, :email, :token, uniqueness: true
+  validates :password, length: { minimum: 8 }, allow_nil: true
 
   has_many :posts
-
-  def regenerate_token
-    self.token = self.class.generate_unique_secure_token
-    self.save!(validate: false)
-
-    self
-  end
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,12 +1,39 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test 'it can regenerate the token' do
-    user = create(:user)
-    original_token = user.token
+  test 'a new user is not valid without a password' do
+    user = User.new(password: nil)
 
-    user = user.regenerate_token
+    valid = user.valid?
 
-    assert_not_equal original_token, user.token
+    assert_equal false, valid
+  end
+
+  test 'a new user is not valid with a short password' do
+    user = User.new(password: 'short')
+
+    valid = user.valid?
+
+    assert_equal false, valid
+  end
+
+  test 'a user is valid with no changes' do
+    create(:user)
+    user = User.first
+
+    valid = user.valid?
+
+    assert_equal true, valid
+  end
+
+  test 'an existing user is valid with a new (valid) password' do
+    create(:user)
+    user = User.first
+    user.password = 'New Password'
+    user.save!
+
+    valid = user.valid?
+
+    assert_equal true, valid
   end
 end
