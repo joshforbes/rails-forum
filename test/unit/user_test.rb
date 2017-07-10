425c53e27054967a845de874a916eb96abc6592c
Josh Forbes <jforbes@kimmel.com>
2017-07-09 23:25:57 -0400

    Cleaning up tests by replacing assert_equal true with just assert

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -4,26 +4,20 @@ class UserTest < ActiveSupport::TestCase
   test 'a new user is not valid without a password' do
     user = User.new(password: nil)
 
-    valid = user.valid?
-
-    assert_equal false, valid
+    refute user.valid?
   end
 
   test 'a new user is not valid with a short password' do
     user = User.new(password: 'short')
 
-    valid = user.valid?
-
-    assert_equal false, valid
+    refute user.valid?
   end
 
   test 'a user is valid with no changes' do
     create(:user)
     user = User.first
 
-    valid = user.valid?
-
-    assert_equal true, valid
+    assert user.valid?
   end
 
   test 'an existing user is valid with a new (valid) password' do
@@ -32,8 +26,6 @@ class UserTest < ActiveSupport::TestCase
     user.password = 'New Password'
     user.save!
 
-    valid = user.valid?
-
-    assert_equal true, valid
+    assert user.valid?
   end
 end
